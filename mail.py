fcca4f15b3a68348118bae0915a918bde89bba6d
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2022-02-22 18:32:31 +0300

    Now different emails can be viewed

--- mail.py
+++ mail.py
@@ -21,70 +21,72 @@ BOT = telebot.TeleBot(config.TOKEN, parse_mode="MARKDOWN")
 with open("config.json", "r", encoding="utf8") as fp:
     INFO = json.load(fp)
 
-try:
-    with MailBox(config.SERVER).login(
-        config.LOGIN, config.PASSWORD, initial_folder="Inbox"
-    ) as mailbox:
-        week_ago = datetime.date.today() - datetime.timedelta(days=7)
-        for msg in mailbox.fetch(AND(date_gte=week_ago), bulk=True):
-            if int(msg.uid) <= int(INFO["latest"]):
-                continue
-            logging.info("Fetching unseen messages: %s", str(msg.uid))
-            logging.info("Lastest sended message is: %s", str(INFO["latest"]))
-            title_text = msg.subject
-            from_text = msg.from_
+for chat, info in INFO.items():
+    if chat == "folder":
+        continue
+    try:
+        with MailBox(config.SERVER).login(
+            info["login"], info["password"], initial_folder="Inbox"
+        ) as mailbox:
+            week_ago = datetime.date.today() - datetime.timedelta(days=7)
+            for msg in mailbox.fetch(AND(date_gte=week_ago), bulk=True):
+                if int(msg.uid) <= int(info["latest"]):
+                    continue
+                logging.info("Fetching unseen messages: %s", str(msg.uid))
+                logging.info("Lastest sended message is: %s", str(info["latest"]))
+                title_text = msg.subject
+                from_text = msg.from_
 
-            # Adding symbols for correct MARKDOWN format
-            title_text = title_text.replace("*", "*\\**")
-            title_text = title_text.replace("_", "\\_")
-            title_text = title_text.replace("`", "\\`")
-            title_text = title_text.replace("[", "\\[")
-            title_text = title_text.replace("||", "\\|\\|")
-            from_text = from_text.replace("*", "\\*")
-            from_text = from_text.replace("_", "_\\__")
-            from_text = from_text.replace("`", "\\`")
-            from_text = from_text.replace("[", "\\[")
-            from_text = from_text.replace("||", "\\|\\|")
+                # Adding symbols for correct MARKDOWN format
+                title_text = title_text.replace("*", "*\\**")
+                title_text = title_text.replace("_", "\\_")
+                title_text = title_text.replace("`", "\\`")
+                title_text = title_text.replace("[", "\\[")
+                title_text = title_text.replace("||", "\\|\\|")
+                from_text = from_text.replace("*", "\\*")
+                from_text = from_text.replace("_", "_\\__")
+                from_text = from_text.replace("`", "\\`")
+                from_text = from_text.replace("[", "\\[")
+                from_text = from_text.replace("||", "\\|\\|")
 
-            # Text of message
-            message_text = "_" + from_text + "_\n*" + title_text + "*\n"
-            if not msg.text == "":
-                message_text += "```\n" + msg.text + "\n```"
-            else:
-                message_text += "```\n" + html2text.html2text(msg.html) + "\n```"
-
-            splitted_text = []
-            attachments = []
-            logging.info("Getting attachments")
-            for att in msg.attachments:
-                folder = os.path.join(os.path.normpath(INFO["folder"]), "tmp")
-                if not os.path.isdir(folder):
-                    os.mkdir(folder)
-
-                att_path = os.path.join(folder, att.filename)
-                with open(att_path, "wb") as fp:
-                    fp.write(att.payload)
-
-                if len(attachments) == 0:
-                    attachments.append(att_path)
+                # Text of message
+                message_text = "_" + from_text + "_\n*" + title_text + "*\n"
+                if not msg.text == "":
+                    message_text += "```\n" + msg.text + "\n```"
                 else:
-                    attachments.append(att_path)
-            logging.info(message_text)
-            logging.info(len(attachments))
-            logging.info("Sending message")
-            for chat_id in INFO["chats"]:
+                    message_text += "```\n" + html2text.html2text(msg.html) + "\n```"
+
+                splitted_text = []
+                attachments = []
+                logging.info("Getting attachments")
+                for att in msg.attachments:
+                    folder = os.path.join(os.path.normpath(INFO["folder"]), "tmp")
+                    if not os.path.isdir(folder):
+                        os.mkdir(folder)
+
+                    att_path = os.path.join(folder, att.filename)
+                    with open(att_path, "wb") as fp:
+                        fp.write(att.payload)
+
+                    if len(attachments) == 0:
+                        attachments.append(att_path)
+                    else:
+                        attachments.append(att_path)
+                logging.info(message_text)
+                logging.info(len(attachments))
+                logging.info("Sending message")
                 if len(attachments) == 0:
                     splitted_text = telebot.util.smart_split(
                         message_text, chars_per_string=4000
                     )
                     if len(splitted_text) > 1:
                         BOT.send_message(
-                            chat_id, splitted_text[0] + "\n```", parse_mode="MARKDOWN"
+                            chat,
+                            splitted_text[0] + "\n```",
+                            parse_mode="MARKDOWN",
                         )
                     else:
-                        BOT.send_message(
-                            chat_id, splitted_text[0], parse_mode="MARKDOWN"
-                        )
+                        BOT.send_message(chat, splitted_text[0], parse_mode="MARKDOWN")
                 else:
                     splitted_text = telebot.util.smart_split(
                         message_text, chars_per_string=1000
@@ -112,17 +114,20 @@ try:
                             media_group.append(
                                 telebot.types.InputMediaDocument(open(pth, "rb"))
                             )
-                    BOT.send_media_group(chat_id, media_group)
+                    BOT.send_media_group(chat, media_group)
                 for text in splitted_text[1:-1]:
                     BOT.send_message(
-                        chat_id, "```\n" + text + "\n```", parse_mode="MARKDOWN"
+                        chat, "```\n" + text + "\n```", parse_mode="MARKDOWN"
                     )
                 if len(splitted_text) > 1:
                     BOT.send_message(
-                        chat_id, "```\n" + splitted_text[-1], parse_mode="MARKDOWN"
+                        chat, "```\n" + splitted_text[-1], parse_mode="MARKDOWN"
                     )
-            INFO["latest"] = int(msg.uid)
-            with open("config.json", "w", encoding="utf8") as fp:
-                json.dump(INFO, fp, ensure_ascii=False)
-except Exception as err:
-    logging.error(str(err))
+                media_group.clear()
+                for attachment in attachments:
+                    os.remove(attachment)
+                INFO[chat]["latest"] = int(msg.uid)
+                with open("config.json", "w", encoding="utf8") as fp:
+                    json.dump(INFO, fp, ensure_ascii=False)
+    except Exception as err:
+        logging.error(str(err))
--- telegram.py
+++ telegram.py
@@ -17,47 +17,78 @@ with open("config.json", "r", encoding="utf8") as fp:
     DATABASE = json.load(fp)
 
 
+def check_for_credentials(message):
+    """
+    Check if message is answer to login request message.
+    """
+    if message.text is not None and len(message.text.split(" ")) == 2:
+        try:
+            return message.reply_to_message.text == config.LOGIN
+        except AttributeError:
+            return False
+    return False
+
+
 def update_database():
     with open("config.json", "w", encoding="utf8") as fp:
         json.dump(DATABASE, fp, ensure_ascii=False)
 
 
-@BOT.message_handler(commands=["start", "help"])
-def start_private(message):
-    if message.chat.id not in DATABASE["chats"]:
-        DATABASE["chats"].append(message.chat.id)
+@BOT.message_handler(func=check_for_credentials)
+def login(message):
+    if str(message.chat.id) not in DATABASE.keys():
+        DATABASE[str(message.chat.id)] = {
+            "latest": 0,
+            "password": message.text.split(" ")[1],
+            "login": message.text.split(" ")[0],
+        }
         update_database()
     BOT.reply_to(message, config.HELP)
+
+
+@BOT.channel_post_handler(func=check_for_credentials)
+def login_channel(message):
+    if str(message.chat.id) not in DATABASE.keys():
+        DATABASE[str(message.chat.id)] = {
+            "latest": 0,
+            "password": message.text.split(" ")[1],
+            "login": message.text.split(" ")[0],
+        }
+        update_database()
+    BOT.reply_to(message, config.HELP)
+
+
+@BOT.message_handler(commands=["start", "help"])
+def start_private(message):
+    BOT.reply_to(message, config.LOGIN)
 
 
 @BOT.channel_post_handler(commands=["start", "help"])
 def start_channel(message):
-    if message.chat.id not in DATABASE["chats"]:
-        DATABASE["chats"].append(message.chat.id)
-        update_database()
-    BOT.reply_to(message, config.HELP)
+    BOT.reply_to(message, config.LOGIN)
 
 
 @BOT.message_handler(commands=["stop"])
 def stop_private(message):
-    if message.chat.id in DATABASE["chats"]:
-        DATABASE["chats"].remove(message.chat.id)
+    if str(message.chat.id) in DATABASE.keys():
+        DATABASE.remove(str(message.chat.id))
         update_database()
     BOT.reply_to(message, config.STOP)
 
 
 @BOT.channel_post_handler(commands=["stop"])
 def stop_channel(message):
-    if message.chat.id in DATABASE["chats"]:
-        DATABASE["chats"].remove(message.chat.id)
+    if str(message.chat.id) in DATABASE.keys():
+        DATABASE.remove(str(message.chat.id))
         update_database()
     BOT.reply_to(message, config.STOP)
 
 
 commands = []
-for command in config.COMMANDS.keys():
-    commands.append(telebot.types.BotCommand(command, config.COMMANDS[command]))
+for command, info in config.COMMANDS.items():
+    commands.append(telebot.types.BotCommand(command, info))
 
 BOT.set_my_commands(commands)
 
+BOT.remove_webhook()
 BOT.polling(non_stop=True)
